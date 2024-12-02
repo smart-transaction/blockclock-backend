281a56abe23955fd719ce6a2c1fdd8984badb767
Evgeny Koshelev <eko2000@gmail.com>
2024-12-02 15:36:32 +0100

    Initial app skeleton

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use axum::{routing::get, serve, Router};
+use clap::Parser;
+use tokio::net::TcpListener;
+
+#[derive(Parser, Debug)]
+pub struct Args {
+    #[arg(long, default_value_t = 8000)]
+    pub port: u16,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    let app = Router::new().route("/", get(|| async { "Blockclock Backend" }));
+
+    let tcp_listener = TcpListener::bind(format!("0.0.0.0:{}", args.port))
+        .await
+        .unwrap();
+
+    println!("Starting server at port {}", args.port);
+    serve(tcp_listener, app).await.unwrap();
+}
